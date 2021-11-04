c9b592eb9ecc85e39b4020cdbbbd4511bfe91a48
Marcos <gomezredondomarcos@gmail.com>
2021-11-04 05:19:41 -0300

    se corrige el calculo de THD

--- calcularMSEyTHD.m
+++ calcularMSEyTHD.m
@@ -20,25 +20,43 @@ for k=1:length(d)
     ref = table2array(T(17:25016, 2));
     out = table2array(T(17:25016, 4));
 
-    time = str2double(time);
-    ref = str2double(ref);
-    out = str2double(out);
+    %time = str2double(time);
+    %ref = str2double(ref);
+    %out = str2double(out);
 
     %MSE
     MSE = sum((ref-out).^2)/25000
 
     %THD de osvaldo
-    MA = max(ref);
-    %ventana
-    first_max = find(out==MA, 1, 'first');
-    last_max = find(out==MA, 1, 'last');
-    Arms=MA/sqrt(2);
-    Amedrms=rms(out(first_max:last_max));  %(first_max:last_max) si se usa ventana
-    Adisrms=sqrt((abs(Amedrms^2-Arms^2)));
-    THD=Adisrms/Arms*100
+%     B0 = mean(out);  % Vertical shift
+%     B1 = (max(out) - min(out))/2; % Amplitude
+%     B2 = 2*pi*50; % Phase (Number of peaks)
+%     B3 = pi/2; % Phase shift (eyeball the Curve)
+%     myFit = NonLinearModel.fit(time,out, 'y ~ b0 + b1*sin(b2*x1 + b3)', [B0, B1, B2, B3]);
+%     hold on
+%     plot(time, myFit.Fitted);
+%     hold off
+%     %ventana
+%     first = find(time==0.12, 1, 'first');
+%     last = find(time==0.12, 1, 'first');
+% 
+%     Amedrms=rms(out(first:last));
+%     Arefrms=rms(ref(first:last));  %(first_max:last_max) si se usa ventana
+%     A1rms=table2array(myFit.Coefficients(2,1))/sqrt(2);
+%     Adisrms=sqrt((abs(Amedrms^2-Arefrms^2)));
+%     THDref=Adisrms/A1rms*100
     
-    NN = {f, MSE, THD};
+% thd de matlab
+    [thd_ref_db,harmpow_ref,harmfreq_ref] = thd(ref,1/(time(2)-time(1)),10);
+    [thd_out_db,harmpow_out,harmfreq_out] = thd(out,1/(time(2)-time(1)),10);
+    Tref = table(harmfreq_ref,harmpow_ref,'VariableNames',{'Frequency','Power'}) %100*(10.^(harmpow_ref/20))
+    Tout = table(harmfreq_out,harmpow_out,'VariableNames',{'Frequency','Power'}) %100*(10.^(harmpow_out/20))
+    THDref = 100*(10^(thd_ref_db/20))
+    THDout = 100*(10^(thd_out_db/20))
+    NN = {f, MSE, THDref, THDout};
     M{k,:}= NN;
+    
+
 end
 
 o = strcat(path_out, 'resultado.csv');
@@ -51,3 +69,6 @@ o = strcat(path_out, 'resultadoExcel.xls')
 writetable(T2,o)
 
 o = strcat(path_out, 'resultadoCell.csv');
+
+Source2 = time;
+CH3 =out;
